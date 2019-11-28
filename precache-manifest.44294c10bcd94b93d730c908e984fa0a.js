333f2ca975eb968babbc11351e6594c6c096d70a
s7illen <s7illen@gmail.com>
2019-11-28 11:48:26 -0500

    Updates

---
+++ precache-manifest.44294c10bcd94b93d730c908e984fa0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99034a7ebd250b5127348acad5a0e09b",
+    "url": "/Steven-Chen-Project-5/index.html"
+  },
+  {
+    "revision": "7d4a474336afde3b8928",
+    "url": "/Steven-Chen-Project-5/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "efce2bdaab3410140a66",
+    "url": "/Steven-Chen-Project-5/static/js/2.72b241ab.chunk.js"
+  },
+  {
+    "revision": "7d4a474336afde3b8928",
+    "url": "/Steven-Chen-Project-5/static/js/main.6e9c2d2f.chunk.js"
+  },
+  {
+    "revision": "3f832920ec51001d28d7",
+    "url": "/Steven-Chen-Project-5/static/js/runtime-main.e6102963.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Steven-Chen-Project-5/static/media/logo.25bf045c.svg"
+  }
+]);
