1f21ee2230eb38c8718acd7d464f1601b0cbf0fe
s7illen <s7illen@gmail.com>
2019-12-01 22:16:20 -0500

    Updates

---
+++ precache-manifest.b799dfd5a735c4a8d0724e03e0452a3f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92516b334a52906d5083ff2e367b1a8e",
+    "url": "/Steven-Chen-Project-5/index.html"
+  },
+  {
+    "revision": "f22c63d559e0409b3224",
+    "url": "/Steven-Chen-Project-5/static/css/main.69ade959.chunk.css"
+  },
+  {
+    "revision": "68fcaf1daa81b9b97a51",
+    "url": "/Steven-Chen-Project-5/static/js/2.e9c4368f.chunk.js"
+  },
+  {
+    "revision": "f22c63d559e0409b3224",
+    "url": "/Steven-Chen-Project-5/static/js/main.9eef59a6.chunk.js"
+  },
+  {
+    "revision": "3f832920ec51001d28d7",
+    "url": "/Steven-Chen-Project-5/static/js/runtime-main.e6102963.js"
+  },
+  {
+    "revision": "b7139b392a756f750001479eebef8039",
+    "url": "/Steven-Chen-Project-5/static/media/trump0.b7139b39.gif"
+  },
+  {
+    "revision": "a3bd8ef1811ebf91c5747584dc56049c",
+    "url": "/Steven-Chen-Project-5/static/media/trumpMinus1.a3bd8ef1.gif"
+  },
+  {
+    "revision": "eb2112917e984185243d1ca1551ce085",
+    "url": "/Steven-Chen-Project-5/static/media/trumpMinus2.eb211291.gif"
+  },
+  {
+    "revision": "a970309f0df372d4b21f56a0618a63f0",
+    "url": "/Steven-Chen-Project-5/static/media/trumpMinus3.a970309f.gif"
+  },
+  {
+    "revision": "ff1fb72d996898ac83b35e70d0e6e54c",
+    "url": "/Steven-Chen-Project-5/static/media/trumpPlus1.ff1fb72d.gif"
+  },
+  {
+    "revision": "15630967fc892fd0c99ecdb869c1cea2",
+    "url": "/Steven-Chen-Project-5/static/media/trumpPlus2.15630967.gif"
+  },
+  {
+    "revision": "3215cfc3381dff409f255b13cb219369",
+    "url": "/Steven-Chen-Project-5/static/media/trumpPlus3.3215cfc3.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Steven-Chen-Project-5/precache-manifest.f9b2fa18e3785e1f780fe92cd2351161.js"
+  "/Steven-Chen-Project-5/precache-manifest.b799dfd5a735c4a8d0724e03e0452a3f.js"
 );
 
 self.addEventListener('message', (event) => {
